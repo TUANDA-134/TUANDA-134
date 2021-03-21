f5ee5cd0c6da08220af1d745c7858da473a52277
Tuan <102190296@sv1.dut.udn.vn>
2021-03-21 20:16:13 +0700

    Ai goi toi do

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Phân_tích_thiết_kế_và_giải_thuật
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Ai do co toi hoi");
+        }
+    }
+}
